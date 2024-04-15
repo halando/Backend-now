3dacfef796194f995c3c733517472ed75c25c924
Andó Attila <andoattila698@gmail.com>
2024-04-15 16:32:53 +0200

    first commit

---
+++ Migrations/20240415110523_Event2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTTokenAPI.Migrations
+{
+    public partial class Event2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Event",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Event");
+        }
+    }
+}
